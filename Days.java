e95db2c45c229ca2c13f1eccd77c96f2ce136611
AmanYadav2369 <130037525+AmanYadav2369@users.noreply.github.com>
2023-04-15 15:15:06 +0530

    Add files via upload

---
+++ Days.java
@@ -0,0 +1,27 @@
+/* wajp to print day name of weekend using switch statement.
+*/
+
+import java.io.*;
+class Days
+{
+public static void main(String args[])
+{
+Console c=System.console();
+while(true)
+{
+int day=Integer.parseInt(c.readLine("Enter the no :"));
+switch(day)
+{
+case 1:	System.out.println("Monday");		break;
+case 2:	System.out.println("Tuesday");		break;
+case 3:	System.out.println("Wednesday");	break;
+case 4:	System.out.println("Thursday");	break;
+case 5:  	System.out.println("Friday");		break;
+case 6: 	System.out.println("Saturday");		break;
+case 7:	System.out.println("Sunday");		break;
+default:	System.out.println("Invalid No...");
+
+}
+}
+}
+}
