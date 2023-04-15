a3a658254a7a047ac069e2a2dcf6b753b77f2e51
AmanYadav2369 <130037525+AmanYadav2369@users.noreply.github.com>
2023-04-15 15:16:27 +0530

    Update Days.java

--- Days.java
+++ Days.java
@@ -2,25 +2,20 @@
 */
 
 import java.io.*;
-class Days
-{
-public static void main(String args[])
-{
+class Days{
+public static void main(String args[]){
 Console c=System.console();
-while(true)
-{
+while(true){
 int day=Integer.parseInt(c.readLine("Enter the no :"));
-switch(day)
-{
-case 1:	System.out.println("Monday");		break;
-case 2:	System.out.println("Tuesday");		break;
-case 3:	System.out.println("Wednesday");	break;
-case 4:	System.out.println("Thursday");	break;
-case 5:  	System.out.println("Friday");		break;
+switch(day){
+case 1:	  System.out.println("Monday");		  break;
+case 2:	  System.out.println("Tuesday");		break;
+case 3:	  System.out.println("Wednesday");	break;
+case 4:	  System.out.println("Thursday");	  break;
+case 5:  	System.out.println("Friday");		  break;
 case 6: 	System.out.println("Saturday");		break;
-case 7:	System.out.println("Sunday");		break;
+case 7:	  System.out.println("Sunday");		  break;
 default:	System.out.println("Invalid No...");
-
 }
 }
 }
